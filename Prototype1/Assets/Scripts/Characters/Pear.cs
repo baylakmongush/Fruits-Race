f693d8d439cecfe616c53d4ff5239d2dcf224b43
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-09-22 20:33:47 +0300

    Add mechanics of persons and other persons animation

---
+++ Prototype1/Assets/Scripts/Characters/Pear.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pear : PlayerController
+{
+	public override void LoadAnim()
+	{
+		getAnimator.runtimeAnimatorController = Resources.Load("PlayerAnim/PearStay") as RuntimeAnimatorController;
+	}
+}
---
+++ Prototype1/Assets/Scripts/GetChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetChar : MonoBehaviour
+{
+	private int number;
+	Type randomType;
+
+	void GetClass()
+    {
+		Type[] scriptTypes = { typeof(Pear), typeof(Apple), typeof(Mandarin) };
+		switch (Number)
+		{
+			case 1:
+				randomType = scriptTypes[1];
+				break;
+			case 2:
+				randomType = scriptTypes[2];
+				break;
+			case 3:
+				randomType = scriptTypes[1];
+				break;
+			default:
+				return;
+		}
+	}
+	void Start()
+	{
+		GetClass();
+		gameObject.AddComponent(randomType);
+	}
+
+	public int Number
+	{
+		get => this.number;
+		set => this.number = value;
+	}
+}
